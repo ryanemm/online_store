4c61e65d3e8426c2bc081e78b0a1207bfafe49f8
ryanemm <59361481+ryanemm@users.noreply.github.com>
2021-08-24 23:58:08 +0200

    changed firebase initialize code

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,48 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
+import "package:firebase_core/firebase_core.dart";
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: LandingPage(),
-    );
-  }
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(App());
 }
 
-class LandingPage extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
   final Future<FirebaseApp> _initialization = Firebase.initializeApp();
 
   @override
   Widget build(BuildContext context) {
-    Scaffold(
-        body: Container(
-      child: Center(child: Text("Hello World!")),
-    ));
     return FutureBuilder(
+      //initialize FlutterFire
       future: _initialization,
       builder: (context, snapshot) {
-        // if Snapshot has error
+        //check for errors
         if (snapshot.hasError) {
-          return Scaffold(
+          return  MaterialApp ( home:  
+          	Scaffold(
             body: Center(
-              child: Text("Error: ${snapshot.error}"),
+              child: Text("Connection error"),
             ),
-          );
-        }
-        if (snapshot.connectionState == ConnectionState.done) {
-          Scaffold(
-              body: Container(
-            child: Center(child: Text("Firebase App Initialized")),
           ));
         }
-
-        return Scaffold(
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp ( home:
+          Scaffold(
+            body: Center(
+              child: Text("Firebase initialized"),
+            ),
+          ));
+        }
+        return MaterialApp ( home:
+        Scaffold(
           body: Center(
-            child: Text("Initializing App"),
+            child: Text("Loading"),
           ),
-        );
+        ));
       },
     );
   }
---
+++ lib/src/main2.dart
@@ -0,0 +1,51 @@
+/*import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LandingPage(),
+    );
+  }
+}
+
+class LandingPage extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    Scaffold(
+        body: Container(
+      child: Center(child: Text("Hello World!")),
+    ));
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        // if Snapshot has error
+        if (snapshot.hasError) {
+          return Scaffold(
+            body: Center(
+              child: Text("Error: ${snapshot.error}"),
+            ),
+          );
+        }
+        if (snapshot.connectionState == ConnectionState.done) {
+          Scaffold(
+              body: Container(
+            child: Center(child: Text("Firebase App Initialized")),
+          ));
+        }
+
+        return Scaffold(
+          body: Center(
+            child: Text("Initializing App"),
+          ),
+        );
+      },
+    );
+  }
+}
+*/
